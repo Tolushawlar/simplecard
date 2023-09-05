b604cca5601763421f4e5c142b655859629247b3
Tolushawlar <tolushaw.it@gmail.com>
2023-09-05 09:39:12 +0100

    cards update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Card from './components/Card';
-import './App.css';
+import Card from "./components/Card";
+import "./App.css";
+import userData from "./userData";
 
-function App() {
-  return (
-    <Card/>
-  );
+export default function App() {
+  const userDisplay = userData.map((user) => {
+    return (
+      <Card
+        key={user.id}
+        name={user.name}
+        phrase={user.company.catchPhrase}
+        company={user.company.name}
+        zipcode={user.address.zipcode}
+      />
+    );
+  });
+
+  return <div className="App">{userDisplay}</div>;
 }
-
-export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,21 @@
 import React from "react";
-import UserImg from "../undraw_Male_avatar_g98d.png";
+import Eth from "../images/icon-ethereum.svg";
+import Time from "../images/icon-clock.svg"
 import "../Card.css";
 
-
-export default function Card() {
+export default function Card(props) {
+  const imageSrc = `https://random.imagecdn.app/500/${props.zipcode.slice(-3)}`;
   return (
     <div className="card">
-      <img src={UserImg} alt="Card" width="50px" />
+      <img src={imageSrc} alt="Card" />
       <div className="card-content">
-        <h2>Olusola Itunu</h2>
-        <p>Fullstack software Develpoer.</p>
+        <h2>{props.name}</h2>
+        <p className="phrase">{props.phrase}.</p>
+        <hr />
+        <p className="company">
+          <img src={imageSrc} alt="profile" />
+          Engineer At: <span> {props.company}</span>
+        </p>
       </div>
     </div>
   );
